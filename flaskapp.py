302e9e225b83a39fee534609d51aa05e2f23be4b
sgomeza13 <sgomeza13@eafit.edu.co>
2022-11-20 09:01:03 -0500

    updated controller input

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import time
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
+
+@app.route('/adelante')
+def adelante():
+    print("adelante")
+    return ("true")
+
+@app.route('/atras')
+def atras():
+    print("atras")
+    return ("true")
+
+@app.route('/izquierda')
+def izquierda():
+    print("izquierda")
+    return ("true")
+    
+@app.route('/derecha')
+def derecha():
+    print("derecha")
+    return ("True")
+
+@app.route('/detener')
+def detener():
+    print("detener")
+    return ("true")
+            
 app.run(host= "0.0.0.0", port= 8000)
+
