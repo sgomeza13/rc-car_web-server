f15ebe803661d8be6c372c47cde7b9a6d59dab55
sgomeza13 <sgomeza13@eafit.edu.co>
2022-11-21 14:26:53 -0500

    random graph

--- flaskapp.py
+++ flaskapp.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 from datetime import time
-
+import random
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    legend = 'Temperatura'
+    labels = [  '12:00PM', '12:10PM', '12:20PM', '12:30PM', '12:40PM', '12:50PM',
+                '1:00PM', '1:10PM', '1:20PM', '1:30PM', '1:40PM', '1:50PM',
+                '2:00PM', '2:10PM', '2:20PM', '2:30PM', '2:40PM', '2:50PM']
+    values = [random.randint(10,40) for _ in range(17)]
     return render_template("index.html", values=values, labels=labels, legend=legend)
 
 @app.route('/adelante')
